869e5f07e1f7db47d55f4aa82a99d2fbff7bdb8c
KacimSultan <ouss6561@gmail.com>
2026-03-29 21:00:53 +0100

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,59 @@
+import telebot
+import os
+
+TOKEN = os.getenv("TOKEN")
+
+bot = telebot.TeleBot(TOKEN)
+
+import telebot
+import os
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+TOKEN = os.getenv("TOKEN")
+
+bot = telebot.TeleBot(TOKEN)
+
+
+# -------- START --------
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "👋 Bienvenue ! Tape /menu")
+
+
+# -------- HELP --------
+@bot.message_handler(commands=['help'])
+def help_command(message):
+    bot.reply_to(message, """
+📌 Commandes:
+/start
+/help
+/menu
+""")
+
+
+# -------- MENU AVEC BOUTONS --------
+@bot.message_handler(commands=['menu'])
+def menu(message):
+    markup = InlineKeyboardMarkup()
+
+    btn1 = InlineKeyboardButton("ℹ️ Info", callback_data="info")
+    btn2 = InlineKeyboardButton("👋 Salut", callback_data="hello")
+
+    markup.add(btn1, btn2)
+
+    bot.send_message(message.chat.id, "Choisis une option :", reply_markup=markup)
+
+
+# -------- ACTION BOUTONS --------
+@bot.callback_query_handler(func=lambda call: True)
+def callback(call):
+
+    if call.data == "info":
+        bot.send_message(call.message.chat.id, "Je suis un bot amélioré 🚀")
+
+    elif call.data == "hello":
+        bot.send_message(call.message.chat.id, "Salut 😄")
+
+
+print("Bot is running...")
+bot.infinity_polling(skip_pending=True)
